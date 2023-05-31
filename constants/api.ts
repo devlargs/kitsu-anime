8fada15c86c42165b293d2cc15559394f9013e44
Ralph Largo <devlargs@gmail.com>
2023-06-01 00:09:28 +0800

    feat: added multifilter functionality

---
+++ constants/api.ts
@@ -0,0 +1,1 @@
+export const API_URL = 'https://kitsu.io/api/edge/anime?page[limit]=10&page[offset]=0';
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
-import { Box, Flex, Grid, GridItem, Input, InputGroup, InputLeftElement, Text } from '@chakra-ui/react';
+import { Box, Flex, Grid, GridItem, Input, InputGroup, InputLeftElement, Spinner, Text } from '@chakra-ui/react';
+import { API_URL } from '@constants/api';
+import { PLACEHOLDER_BLUR } from '@constants/images';
 import { STYLES } from '@constants/styles';
 import uniqBy from 'lodash/uniqBy';
 import { NextPage } from 'next';
@@ -7,13 +9,26 @@ import { useEffect, useMemo, useState } from 'react';
 import { AiFillHeart, AiFillStar, AiOutlineHeart, AiOutlineSearch, AiOutlineStar } from 'react-icons/ai';
 
 type AnimeFilters = 'starred' | 'favorited';
+type AnimeData = {
+  id: string;
+  attributes: {
+    canonicalTitle: string;
+    posterImage: {
+      large: string;
+    };
+    averageRating: number;
+    popularityRank: number;
+  };
+};
 
 const Home: NextPage = () => {
-  const [data, setData] = useState<Record<string, any>[]>([]);
+  const [data, setData] = useState<AnimeData[]>([]);
   const [loading, setLoading] = useState(false);
   const [, setNextPage] = useState('');
   const [searchText, setSearchText] = useState('');
   const [filters, setFilters] = useState<AnimeFilters[]>([]);
+  const [starred, setStarred] = useState<string[]>([]);
+  const [favorited, setFavorited] = useState<string[]>([]);
 
   const load = async (url: string): Promise<void> => {
     try {
@@ -22,6 +37,8 @@ const Home: NextPage = () => {
       const json = await res.json();
       setNextPage(json.links.next);
       setData((prev) => uniqBy([...prev, ...json.data], 'id'));
+      setFilters([]);
+      setSearchText('');
     } catch (ex) {
       console.error(ex);
     } finally {
@@ -29,17 +46,45 @@ const Home: NextPage = () => {
     }
   };
 
-  const filteredData = useMemo(() => {
-    return data.filter((item) => item.attributes.canonicalTitle.toLowerCase().includes(searchText.toLowerCase()));
-  }, [searchText, data]);
-
-  useEffect(() => {
-    void load('https://kitsu.io/api/edge/anime?page[limit]=10&page[offset]=0');
-  }, []);
-
   const chooseFilter = (filter: AnimeFilters): void => {
     setFilters((prev) => (prev.includes(filter) ? prev.filter((item) => item !== filter) : [...prev, filter]));
   };
+
+  const toggleAnime = (filter: AnimeFilters, id: string): void => {
+    (filter === 'starred' ? setStarred : setFavorited)((prev) => {
+      const temp = prev.includes(id) ? prev.filter((item) => item !== id) : [...prev, id];
+      localStorage.setItem(filter, JSON.stringify(temp));
+      return temp;
+    });
+  };
+
+  const filteredData = useMemo(() => {
+    if (!filters.length && !searchText) return data;
+
+    return data.filter((item) => {
+      const isSearchMatch = item.attributes.canonicalTitle.toLowerCase().includes(searchText.toLowerCase());
+
+      if (filters.includes('starred')) {
+        return isSearchMatch && starred.includes(item.id);
+      }
+
+      if (filters.includes('favorited')) {
+        return isSearchMatch && favorited.includes(item.id);
+      }
+
+      return isSearchMatch;
+    });
+  }, [searchText, data, filters, favorited, starred]);
+
+  useEffect(() => {
+    void load(API_URL);
+
+    const localStar = localStorage.getItem('starred');
+    const localFavorite = localStorage.getItem('favorited');
+
+    if (localStar) setStarred(JSON.parse(localStar));
+    if (localFavorite) setFavorited(JSON.parse(localFavorite));
+  }, []);
 
   return (
     <Box px={STYLES.container.padding} my="2rem">
@@ -87,7 +132,7 @@ const Home: NextPage = () => {
         <Text fontSize="20px">{filteredData.length} Results</Text>
       </Flex>
 
-      {data.length ? (
+      {filteredData.length ? (
         <Grid
           templateColumns={{
             base: 'repeat(1, 1fr)',
@@ -112,6 +157,8 @@ const Home: NextPage = () => {
                   width={550}
                   height={780}
                   alt={item.attributes.canonicalTitle}
+                  placeholder="blur"
+                  blurDataURL={PLACEHOLDER_BLUR}
                 />
                 <Box p="10px">
                   <Text
@@ -132,13 +179,15 @@ const Home: NextPage = () => {
                     }}
                   >
                     <Flex alignItems="center" gap="4px">
-                      <AiOutlineStar color="white" />
-                      <AiFillStar color="white" />
+                      <Box onClick={(): void => toggleAnime('starred', item.id)} cursor="pointer">
+                        {starred.includes(item.id) ? <AiFillStar color="white" /> : <AiOutlineStar color="white" />}
+                      </Box>
                       <Text color="white">{item.attributes.averageRating}</Text>
                     </Flex>
                     <Flex alignItems="center" gap="4px">
-                      <AiOutlineHeart color="white" />
-                      <AiFillHeart color="white" />
+                      <Box onClick={(): void => toggleAnime('favorited', item.id)} cursor="pointer">
+                        {favorited.includes(item.id) ? <AiFillHeart color="white" /> : <AiOutlineHeart color="white" />}
+                      </Box>
                       <Text color="white">{item.attributes.popularityRank}</Text>
                     </Flex>
                   </Flex>
@@ -151,7 +200,11 @@ const Home: NextPage = () => {
         <p>No data</p>
       )}
 
-      {loading && <p>Loading...</p>}
+      {loading && (
+        <Box>
+          <Spinner />
+        </Box>
+      )}
     </Box>
   );
 };
